503d928e26f4dd4173b11abfd332fbd805750d74
ProCryptor <96250392+ProCryptor@users.noreply.github.com>
2025-12-23 21:52:40 +0100

    Create chain_modules.py

---
+++ src/utils/chain_modules.py
@@ -0,0 +1,27 @@
+CHAIN_MODULES = {
+    'BASE': [
+        'UNISWAP',
+        'MATCHA_SWAP',
+        'BUNGEE_SWAP',
+        'OWLTO_SWAP',
+        'RUBYSCORE_VOTE',
+        'WRAPPER_UNWRAPPER',
+        'CONTRACT_DEPLOY'
+    ],
+    'ARBITRUM': [
+        'UNISWAP',
+        'MATCHA_SWAP',
+        'BUNGEE_SWAP'
+    ],
+    'OPTIMISM': [
+        'UNISWAP',
+        'MATCHA_SWAP'
+    ],
+    'LINEA': [
+        'UNISWAP',
+        'MATCHA_SWAP'
+    ],
+    'ETHEREUM': [
+        'UNISWAP'
+    ]
+}
