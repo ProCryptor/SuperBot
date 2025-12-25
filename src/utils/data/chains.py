47697c5b2171ac1e04f5e6548d84bbc53db3b944
ProCryptor <96250392+ProCryptor@users.noreply.github.com>
2025-12-25 22:31:42 +0100

    Update chains.py

--- src/utils/data/chains.py
+++ src/utils/data/chains.py
@@ -1,58 +1,34 @@
-# src/utils/data/chains.py
-from pydantic import BaseModel
-
-class Chain(BaseModel):
-    chain_name: str
-    native_token: str
-    rpc: str
-    chain_id: int
-    scan: str = ""  # Добавляем scan, если нужно (опционально)
-
-# Создаём экземпляры (Pydantic автоматически проверит типы)
-BASE = Chain(
-    chain_name='BASE',
-    native_token='ETH',
-    rpc='https://mainnet.base.org',
-    chain_id=8453,
-    scan='https://basescan.org/tx'
-)
-
-OPTIMISM = Chain(
-    chain_name='OPTIMISM',
-    native_token='ETH',
-    rpc='https://mainnet.optimism.io',
-    chain_id=10,
-    scan='https://optimistic.etherscan.io/tx'
-)
-
-ARBITRUM = Chain(
-    chain_name='ARBITRUM',
-    native_token='ETH',
-    rpc='https://arb1.arbitrum.io/rpc',
-    chain_id=42161,
-    scan='https://arbiscan.io/tx'
-)
-
-ETHEREUM = Chain(
-    chain_name='ETHEREUM',
-    native_token='ETH',
-    rpc='https://rpc.ankr.com/eth',
-    chain_id=1,
-    scan='https://etherscan.io/tx'
-)
-
-LINEA = Chain(
-    chain_name='LINEA',
-    native_token='ETH',
-    rpc='https://rpc.linea.build',
-    chain_id=59144,
-    scan='https://lineascan.build/tx'
-)
+from src.models.chain import Chain
 
 chain_mapping = {
-    'BASE': BASE,
-    'OPTIMISM': OPTIMISM,
-    'ARBITRUM': ARBITRUM,
-    'ETHEREUM': ETHEREUM,
-    'LINEA': LINEA,
+    'BASE': Chain(
+        chain_name='BASE',
+        native_token='ETH',
+        rpc='https://mainnet.base.org',
+        chain_id=8453
+    ),
+    'ARBITRUM': Chain(
+        chain_name='ARBITRUM',
+        native_token='ETH',
+        rpc='https://arb1.arbitrum.io/rpc',
+        chain_id=42161
+    ),
+    'OPTIMISM': Chain(
+        chain_name='OPTIMISM',
+        native_token='ETH',
+        rpc='https://mainnet.optimism.io',
+        chain_id=10
+    ),
+    'ETHEREUM': Chain(
+        chain_name='ETHEREUM',
+        native_token='ETH',
+        rpc='https://rpc.ankr.com/eth',
+        chain_id=1
+    ),
+    'LINEA': Chain(
+        chain_name='LINEA',
+        native_token='ETH',
+        rpc='https://rpc.linea.build',
+        chain_id=59144
+    ),
 }
