839751880aed17eba989930bd5fc1b357083962e
ProCryptor <96250392+ProCryptor@users.noreply.github.com>
2025-12-25 17:38:11 +0100

    Update process_chain_disperse.py

--- src/utils/bridges/process_chain_disperse.py
+++ src/utils/bridges/process_chain_disperse.py
@@ -1,6 +1,7 @@
 import random
 from loguru import logger
 from src.utils.planner import ActivityPlanner
+from src.utils.data.bridges import BRIDGES
 
 async def process_chain_disperse(route):
     planner = ActivityPlanner()
