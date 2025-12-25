36d7a1e696e0a0439c50ed4c4dfbbb7abe3b3822
ProCryptor <96250392+ProCryptor@users.noreply.github.com>
2025-12-25 13:49:33 +0100

    Update chain_modules.py

--- src/utils/chain_modules.py
+++ src/utils/chain_modules.py
@@ -1,29 +1,34 @@
-from src.modules.swaps.uniswap.uniswap import process_uniswap
+from src.modules.swaps.uniswap.uniswap import Uniswap
+from src.modules.swaps.matcha.matcha import Matcha
+# дальше по аналогии
+
+async def process_uniswap(route, chain):
+    u = Uniswap(
+        private_key=route.wallet.private_key,
+        proxy=route.wallet.proxy,
+        from_token='ETH',
+        to_token='USDC',
+        amount=0.1,
+        use_percentage=True,
+        swap_percentage=0.05,
+        swap_all_balance=False,
+        chain=chain
+    )
+    return await u.swap()
+
+async def process_matcha(route, chain):
+    m = Matcha(...)
+    return await m.swap()
+
 
 CHAIN_MODULES = {
     'BASE': [
         'UNISWAP',
         'MATCHA_SWAP',
-        'BUNGEE_SWAP',
-        'OWLTO_SWAP',
-        'RUBYSCORE_VOTE',
-        'WRAPPER_UNWRAPPER',
-        'CONTRACT_DEPLOY'
     ],
-    'ARBITRUM': [
-        'UNISWAP',
-        'MATCHA_SWAP',
-        'BUNGEE_SWAP'
-    ],
-    'OPTIMISM': [
-        'UNISWAP',
-        'MATCHA_SWAP'
-    ],
-    'LINEA': [
-        'UNISWAP',
-        'MATCHA_SWAP'
-    ],
-    'ETHEREUM': [
-        'UNISWAP'
-    ]
+}
+
+MODULE_HANDLERS = {
+    'UNISWAP': process_uniswap,
+    'MATCHA_SWAP': process_matcha,
 }
